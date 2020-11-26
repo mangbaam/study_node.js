8ebd6a8dce22043637bd6395c94020b0d1794691
mangbaam <pmb0836@gmail.com>
2020-11-27 01:06:06 +0900

    dynamic web page

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 var express = require('express');   // 설치한 express module을 불러와서 변수(express)에 담는다.
 var app = express();    // express를 실행하여 app object를 초기화 한다.
 
-app.get('/', function(req, res) {   // '/' 위치에 'get' 요청을 받는 경우
-    res.send('Hello world');        // "Hello world"를 보낸다.
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req, res) {
+    res.render('hello', {name:req.query.nameQuery});
 });
 
-var port = 5000;    // 사용할 포트 번호
+app.get('/hello/:nameParam', function(req, res) {
+    res.render('hello', {name:req.params.nameParam});
+});
+
+var port = 5000;
 app.listen(port, function() {
     console.log('server on! http://localhost:'+port);
 });
